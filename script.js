bb9dfefa13f91f3e255a6ea163d66a3f6980e5d7
Aashish Lamba <136936545+AashishLamba@users.noreply.github.com>
2026-06-03 08:14:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const todo = document.querySelector("#todo");
+const progress = document.querySelector("#progress");
+const done = document.querySelector("#done");
+
+const tasks = document.querySelectorAll(".task");
+
+tasks.forEach((task) => {
+  task.addEventListener("dragstart", (e) => {
+    
+  });
+});
+
+function addDragEventsOnColumn(column) {
+  column.addEventListener("dragenter", (e) => {
+    e.preventDefault();
+    column.classList.add("hover-over");
+  });
+
+  column.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    column.classList.remove("hover-over");
+  });
+}
+
+addDragEventsOnColumn(todo);
+addDragEventsOnColumn(progress);
+addDragEventsOnColumn(done);
